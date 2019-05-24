6756bcb7065bfc982346e1a5bc2a28bfc06d054e
Gøran Strønstad <goran.hovde@gmail.com>
2019-05-24 15:52:49 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
+# Rails route names convention says to use _path,
+# unless you are redirecting, in which case use _url
+#
+# Using the pattern: get '/page', to: 'static_pages#page'
+# creates two named routed: (1) page_path, and (2) page_url
+#
+
 Rails.application.routes.draw do
+  get 'users/new'
+  get '/signup',  to: 'users#new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about',   to: 'static_pages#about'
+  get '/help',    to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
 end
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    # Rails automatically inserts the value of about_path in place of the question mark
+    # count: 2 verifies that there are two such links (one each for the logo and navigation),
+    # this makes sure that both links to the Home page exist
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+  end
+
+end
